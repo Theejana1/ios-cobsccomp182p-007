4e2b92f0609687684ccd245d46c5f347c61702b0
Theejana1 <59130226+Theejana1@users.noreply.github.com>
2020-02-06 21:28:10 +0530

    Initial Commit

---
+++ NIBM Event/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM Event
+//
+//  Created by Yanik Perera on 2/6/20.
+//  Copyright © 2020 Yanik Perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
